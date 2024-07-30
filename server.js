f517a1b09c0381338a98bdb0ac27d819bccb7dde
Jonathan C <johnnymilkshakes@icloud.com>
2024-07-30 16:30:49 -0400

    fully functional

--- server.js
+++ server.js
@@ -4,11 +4,14 @@ import express from "express"
 import dotenv from "dotenv" // require package
 import mongoose from "mongoose" 
 import Fruit from "./models/fruit.js"
+import methodOverride from "method-override"
+import logger from "morgan"
 
 dotenv.config(); // Loads the environment variables from .env file
 const app = express();
 app.use(express.urlencoded({ extended: false }));
-
+app.use(methodOverride("_method")); 
+app.use(logger("dev")); 
 
 mongoose.connect(process.env.MONGODB_URI);
 
@@ -24,13 +27,17 @@ app.get("/fruits/new", async (req, res) => {
 
 app.get("/fruits", async (req, res) => {
     const allFruits = await Fruit.find();
-    console.log(allFruits)
-
     res.render("fruits/index.ejs", {fruits: allFruits});
   });
+
+  app.get('/fruits/:id', async (req, res) => {
+
+    const foundFruit = await Fruit.findById(req.params.id)
+
+    res.render("fruits/show.ejs", {fruit: foundFruit})
+  })
   
 app.post('/fruits', async (req, res) => {
-    console.log(req.body)
     if (req.body.isReadyToEat === "on") {
         req.body.isReadyToEat = true
     } else {
@@ -41,7 +48,29 @@ app.post('/fruits', async (req, res) => {
     res.redirect("/fruits")
 })
 
+app.get("/fruits/:fruitId/edit", async (req, res) => {
+    const foundFruit = await Fruit.findById(req.params.fruitId);
+    res.render(`fruits/edit.ejs`, {fruit: foundFruit});
+  });
+  
+app.put('/fruits/:id', async (req,res) => {
+    if (req.body.isReadyToEat === "on") {
+        req.body.isReadyToEat = true;
+    } else {
+        req.body.isReadyToEat = false;
+    }
+    console.log(await Fruit.findByIdAndUpdate(req.params.id, req.body))
+    res.render("/fruits/edit.ejs")
+})
+
+app.delete('/fruits/:id', async (req,res) => {
+    console.log(await Fruit.findByIdAndDelete(req.params.id))
+    res.redirect("/fruits")
+
+})
+
 mongoose.connection.on("connected", () => {
+    console.clear()
     console.log(`Connected to MongoDB ${mongoose.connection.name}.`)
     app.listen(3000, () => {
         console.log("Listening on port 3000");
